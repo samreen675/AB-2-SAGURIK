63567ea459835877b8f7549e985eef28c7081a11
samreen675 <30996450+samreen675@users.noreply.github.com>
2021-01-25 14:44:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,51 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+ var engine, world;
+var Matter;
+var bgImg;
+ var balloon;
+// var  bgImg=("Hot Air Balloon-01.png")
+ var position;
+ var database;
+function preload(){
+  bgImg=loadImage("Hot Air Ballon-01.png");
+  balloonAnimation=loadAnimation("Hot Air Ballon-02.png","Hot Air Ballon-03.png","Hot Air Ballon-04.png")
 
-var engine, world;
-var box1, pig1;
-var backgroundImg;
-var platform;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
 }
-
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+  createCanvas(800,800);
+    balloon=createSprite(400, 200, 50, 50);
+    balloon.addAnimation("balloonImages", balloonAnimation)
+  
+    // var balloonPosition=database.ref("balloon/height")
+    // balloonPosition.on("value",readPosition,showError)
 
     
-    ground = new Ground(600,height,1200,20)
-    platform = new Ground(100,290,200,200)
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
+  
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
+function draw() {
+  if(bgImg)
+  {
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-    platform.display()
-    bird.display();
+  background(bgImg);
+  }
+   balloon.display();
+  if(keyDown(LEFT_ARROW)){
+    balloon.x=balloon.x-10
+  }
+  if(keyDown(RIGHT_ARROW)){
+    balloon.x=balloon.x+10
+  }
+  if(keyDown(UP_ARROW)){
+    balloon.y=balloon.y-10
+  }
+  if(keyDown(DOWN_ARROW)){
+    balloon.y=balloon.y+10
+  }
+ 
+  
+  drawSprites();
 }
